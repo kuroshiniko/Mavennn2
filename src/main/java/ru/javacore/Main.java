3988b905df3fdc549ae3f8ec94cfd7d216dfc49a
Lenovo <Lenovo@DESKTOP-ULAKLCD>
2023-03-24 12:33:23 +0300

    Hi!

--- src/main/java/ru/javacore/Main.java
+++ src/main/java/ru/javacore/Main.java
@@ -2,11 +2,9 @@ package ru.javacore;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hi");
 
 
-        Tester tester = new Tester("Aleksandr", "Vitushka", 1, "B2", 1100);
-        tester.getFullName();
-        tester.print();
-        tester.print();
+
     }
 }
